b21fd4fe7d4128d06cbf90294ba8b93d18e9f5e3
Jamo <pinguimg@gmail.com>
2024-04-14 11:11:22 +0100

    chore: adding format and save data

--- app/normalizer.py
+++ app/normalizer.py
@@ -1,10 +1,11 @@
 import csv
+import json
 from collections import defaultdict
 
 class FootballEventNormalizer:
-    def __init__(self, input_file='input.csv', output_ext='jsonl'):
-        self.input_file = input_file
-        self.output_ext = output_ext
+    def __init__(self, input='input.csv', output='output.jsonl'):
+        self.input = input
+        self.output = output
         self.matches = defaultdict(dict)
         self.teams = defaultdict(dict)
         self.players = defaultdict(dict)
@@ -13,9 +14,24 @@ class FootballEventNormalizer:
 
     def read_csv_file(self):
         try:
-            with open(self.input_file, 'r') as file:
+            with open(self.input, 'r') as file:
                 return list(csv.DictReader(file))
         except FileNotFoundError:
-            raise FileNotFoundError(f"File {self.input_file} not found.") from e
+            raise FileNotFoundError(f"File {self.input} not found.") from e
         except csv.Error as e:
-            raise csv.Error(f"Error reading CSV file {self.input_file}: {str(e)}") from e
+            raise csv.Error(f"Error reading CSV file {self.input}: {str(e)}") from e
+        
+    def save_to_json_lines(self, data, file_path):
+        try:
+            with open(file_path, 'w') as file:
+                for item in data:
+                    json.dump(item, file)
+                    file.write('\n')
+        except IOError as e:
+            raise IOError(f"Error writing to file {file_path}: {str(e)}") from e
+
+    def save_data(self):
+        self.save_to_json_lines(list(self.matches.values()), f'match.{self.output}')
+        self.save_to_json_lines(list(self.teams.values()), f'team.{self.output}')
+        self.save_to_json_lines(list(self.players.values()), f'player.{self.output}')
+        self.save_to_json_lines(self.statistics, f'statistic.{self.output}')    
