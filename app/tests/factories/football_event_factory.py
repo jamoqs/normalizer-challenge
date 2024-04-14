801d4e18331e294e8ca41815805987f2d56e32a4
Jamo <pinguimg@gmail.com>
2024-04-14 09:48:42 +0100

    test: adding football event factory

---
+++ app/tests/factories/football_event_factory.py
@@ -0,0 +1,30 @@
+import factory
+from faker import Faker
+
+
+fake = Faker()
+
+class EventFactory(factory.Factory):
+    class Meta:
+        model = dict
+
+    match_id = factory.Sequence(lambda n: n)
+    match_name = fake.name()
+    team_id = factory.Sequence(lambda n: n)
+    team_name = fake.name()
+    is_home = fake.boolean()
+    player_id = factory.Sequence(lambda n: n)
+    player_name = fake.name()
+    goals_scored = fake.random_number(digits=2)
+    minutes_played = fake.random_number(digits=2)
+
+class FootBallEventNormalizeFactory(factory.Factory):
+
+    input_file = 'test_input.csv'
+    output_ext = 'jsonl'
+
+    @classmethod
+    def _create(cls, model_class, *args, **kwargs):
+        processor = model_class(*args, **kwargs)
+        processor.run()
+        return processor
