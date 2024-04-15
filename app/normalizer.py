130eeca5585f9308f68a51d881e5949817d5e650
Jamo <pinguimg@gmail.com>
2024-04-15 23:09:54 +0100

    fix: name of keys from csv

--- app/normalizer.py
+++ app/normalizer.py
@@ -7,11 +7,11 @@ class FootballEventNormalizer:
     def __init__(self, input="input.csv", output="output.jsonl"):
         self.input = input
         self.output = output
-        self.matches = defaultdict(dict)
+        self.matches = defaultdict(lambda: defaultdict(str))
         self.teams = defaultdict(dict)
         self.players = defaultdict(dict)
         self.statistics = []
-        self.match_player_stats = defaultdict(dict)
+        self.match_player_stats = defaultdict(lambda: defaultdict(int))
 
     def read_csv_file(self):
         try:
@@ -22,15 +22,15 @@ class FootballEventNormalizer:
         except csv.Error as e:
             raise csv.Error(f"Error reading CSV file {self.input}: {str(e)}") from e
 
-    def process_matches(self, event, is_home):
+    def process_matches(self, event):
         try:
-            match_id = event["Match Id"]
-            team_id = event["Team Id"]
+            match_id = int(event["match_id"])
+            team_id = int(event["team_id"])
             match_data = self.matches[match_id]
             match_data["Match Id"] = match_id
-            match_data["Match Name"] = event["Match Name"]
-            match_data["Home Team Id"] = team_id if is_home else None
-            match_data["Away Team Id"] = team_id if not is_home else None
+            match_data["Match Name"] = event["match_name"]
+            match_data["Home Team Id"] = team_id if event['is_home'] else None
+            match_data["Away Team Id"] = team_id if not event['is_home'] else None
             match_data["Home Goals"] = match_data.get("Home Goals", 0)
             match_data["Away Goals"] = match_data.get("Away Goals", 0)
         except (TypeError, ValueError) as e:
@@ -38,28 +38,28 @@ class FootballEventNormalizer:
 
     def process_teams(self, event):
         try:
-            team_id = event["Team Id"]
+            team_id = int(event["team_id"])
             self.teams[team_id]["Team Id"] = team_id
-            self.teams[team_id]["Team Name"] = event["Team Name"]
+            self.teams[team_id]["Team Name"] = event["team_name"]
         except (TypeError, ValueError) as e:
             raise ValueError(f"Invalid data in event: {event}.") from e
 
     def process_players(self, event):
         try:
-            player_id = event["Player Id"]
-            team_id = event["Team Id"]
+            player_id = int(event["player_id"])
+            team_id = int(event["team_id"])
             self.players[player_id]["Player Id"] = player_id
             self.players[player_id]["Team Id"] = team_id
-            self.players[player_id]["Player Name"] = event["Player Name"]
+            self.players[player_id]["Player Name"] = event["player_name"]
         except (TypeError, ValueError) as e:
             raise ValueError(f"Invalid data in event: {event}.") from e
 
     def process_statistics(self, event):
         try:
-            match_id = event["Match Id"]
-            player_id = event["Player Id"]
-            goals_scored = event["Goals Scored"]
-            minutes_played = event["Minutes Played"]
+            match_id = int(event["match_id"])
+            player_id = int(event["player_id"])
+            goals_scored = int(event["goals_scored"])
+            minutes_played = int(event["minutes_played"])
             match_goals = self.match_player_stats[match_id][player_id] + goals_scored
             self.match_player_stats[match_id][player_id] = match_goals
             fraction_minutes_played = minutes_played / 90
@@ -82,15 +82,14 @@ class FootballEventNormalizer:
         try:
             input_data = self.read_csv_file()
             for event in input_data:
-                is_home = event["Is Home"] == "True"
-                self.process_matches(event, is_home)
+                self.process_matches(event)
                 self.process_teams(event)
                 self.process_players(event)
                 self.process_statistics(event)
 
-                goals_scored = event["Goals Scored"]
-                match_id = event["Match Id"]
-                if is_home:
+                goals_scored = int(event["goals_scored"])
+                match_id = int(event["match_id"])
+                if event['is_home']:
                     self.matches[match_id]["Home Goals"] += goals_scored
                 else:
                     self.matches[match_id]["Away Goals"] += goals_scored
